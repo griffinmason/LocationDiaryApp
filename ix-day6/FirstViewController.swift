dab5268a9ca168852f66a7cd5e9238cf4a462939
griffinmason <griffinmason@ymail.com>
2017-06-05 09:50:34 +0200

    Initial Commit

---
+++ ix-day6/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  ix-day6
+//
+//  Created by George Mason on 6/5/17.
+//  Copyright © 2017 Griffin Mason. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ix-day6/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ix-day6
+//
+//  Created by George Mason on 6/5/17.
+//  Copyright © 2017 Griffin Mason. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
